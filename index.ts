f26f743a2434295ed1da05eda5d25f1ca2170edd
Anatolii Pokhylko <pokhylko@gmail.com>
2021-08-28 21:20:23 +0300

    refactor: remove redundant comments

--- index.ts
+++ index.ts
@@ -1,17 +1,13 @@
-// Require the necessary discord.js classes
 import { Client, Intents } from 'discord.js';
 
-// Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
-// When the client is ready, run this code (only once)
 client.once('ready', () => {
     console.log('Ready!');
 });
 
 const token = process.env.TOKEN;
 
-// Login to Discord with your client's token
 client.login(token);
 
 client.on('messageCreate', (msg) => {
